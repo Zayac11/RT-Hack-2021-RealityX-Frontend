80b82b9522bb0eab134c7b709a7265de400378fa
Zayac11 <brunogrbstudios@gmail.com>
2021-12-03 17:07:15 +0300

    fixed links and time

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,12 +2,12 @@ import {CameraType} from "../types/Types";
 
 export const getCurrentDate = (timestamp: string) => {
     let date = new Date(timestamp)
-    const dd = String(date.getDate()).padStart(2, '0');
-    const mm = String(date.getMonth() + 1).padStart(2, '0');
-    const yyyy = date.getFullYear();
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
+    let yyyy = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = (date.getMinutes()<10?'0':'') + date.getMinutes();
+    let seconds = (date.getSeconds()<10?'0':'') + date.getSeconds();
 
     return mm + '.' + dd + '.' + yyyy + ' ' + hour + ':' + minutes + ':' + seconds
 }
